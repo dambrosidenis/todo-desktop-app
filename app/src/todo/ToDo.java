0c693657069845fc3327703406131884d780ab77
Gabriele Pongelli <pongelligabriele@gmail.com>
2020-10-21 20:52:22 +0200

    Created ToDo class, with 4 members, 2 constructors and the modifyTitle(...) method

--- app/src/todo/ToDo.java
+++ app/src/todo/ToDo.java
@@ -1,5 +1,35 @@
 package app.src.todo;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class ToDo {
-    
-}
+
+    public String title;
+    public String description;
+    public LocalDateTime creation;
+    public ArrayList<String> tags;
+
+    public ToDo(String title, String description, ArrayList<String> tags) {
+        this.title = title;
+        this.description = description;
+        this.creation = LocalDateTime.now();
+        this.tags = tags;
+    }
+
+    public ToDo(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.creation = LocalDateTime.now();
+        this.tags = new ArrayList<String>();
+    }
+
+    /**
+     * MODIFY the title of the todo
+     * @param newTitle is the new title to set
+     */
+    public void modifyTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+}   // class ToDo
