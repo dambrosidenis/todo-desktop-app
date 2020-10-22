4ae787ccef96e883f8a60e0139d929c44e0717af
dambrosidenis <73072997+dambrosidenis@users.noreply.github.com>
2020-10-22 10:54:42 +0200

    Add get() methods to ToDo class
    Added methods to retrieve information about the ToDo instance and changed the name of set() methods; Changed the access level of the methods from public to unspecified to make the methods only public to the same package and the class variables private.

--- app/src/todo/ToDo.java
+++ app/src/todo/ToDo.java
@@ -3,21 +3,21 @@ package app.src.todo;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class ToDo {
+class ToDo {
 
-    public String title;
-    public String description;
-    public LocalDateTime creation;
-    public ArrayList<String> tags;
+    private String title;
+    private String description;
+    private LocalDateTime creation;
+    private ArrayList<String> tags;
 
-    public ToDo(String title, String description, ArrayList<String> tags) {
+    ToDo(String title, String description, ArrayList<String> tags) {
         this.title = title;
         this.description = description;
         this.creation = LocalDateTime.now();
         this.tags = tags;
     }
 
-    public ToDo(String title, String description) {
+    ToDo(String title, String description) {
         this.title = title;
         this.description = description;
         this.creation = LocalDateTime.now();
@@ -28,8 +28,44 @@ public class ToDo {
      * MODIFY the title of the todo
      * @param newTitle is the new title to set
      */
-    public void modifyTitle(String newTitle) {
+    void setTitle(String newTitle) {
         this.title = newTitle;
+    }
+
+    /**
+     * MODIFY the description of the todo
+     * @param newDescription is the new description to set
+     */
+    void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
+
+    /**
+     * RETURN the title of the todo
+     */
+    String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * RETURN the description of the todo
+     */
+    String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * RETURN the date of creation of the todo
+     */
+    LocalDateTime getCreation() {
+        return this.creation;
+    }
+
+    /**
+     * RETURN the tags of the todo
+     */
+    ArrayList<String> getTags() {
+        return this.tags;
     }
 
 }   // class ToDo
