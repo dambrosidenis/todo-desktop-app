e4e5b455b5147cd20910f85983ecc24f3a0cffa1
Gabriele Pongelli <pongelligabriele@gmail.com>
2020-11-11 19:28:41 +0100

    Added maven configuration for JavaFX module and created primary app file.

---
+++ src/main/java/todoapp/App.java
@@ -0,0 +1,17 @@
+package todoapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    public static void main(String[] args) {
+        launch();
+    }
+    
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+}
