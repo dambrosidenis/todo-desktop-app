d11f235f1180811295dce58c924e4a4267e27a8f
dambrosidenis <73072997+dambrosidenis@users.noreply.github.com>
2020-11-18 10:14:13 +0100

    Update Backup.java

--- src/main/java/todoapp/todo/Backup.java
+++ src/main/java/todoapp/todo/Backup.java
@@ -1,16 +1,31 @@
 package todoapp.todo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+
 import todoapp.exceptions.BackupFailedException;
 
-class Backup {
+public class Backup {
+
+    ToDoList currentToDoList;
+    String pathToBackup;
+
+    public Backup(ToDoList currentToDoList, String pathToBackup) {
+        this.currentToDoList = currentToDoList;
+        this.pathToBackup = pathToBackup;
+    }
 
     /**
      * Save the data into the file myBackup.bak.
      * @param currentToDoList valid instance of class ToDoList.
      * @throws BackupFailedException when the backup of the ToDoList fail.
      */
-    static void backupData(ToDoList currentToDoList) throws BackupFailedException {
+    void backupData() throws BackupFailedException {
 
         if (currentToDoList == null) {
             throw new IllegalArgumentException("The ToDoList have to be at least empty.");
@@ -39,7 +54,7 @@ class Backup {
      * @param instanceData is a string array containing the information about the ToDo. Requires to have at least 3 elements.
      * @return the encoded string.
      */
-    private static String encodeToDoToJSON(String[] instanceData) {
+    private String encodeToDoToJSON(String[] instanceData) {
 
         if (instanceData == null || instanceData.length < 3) {
             throw new IllegalArgumentException("The ToDos' string representation must have at least 3 elements.");
@@ -56,7 +71,7 @@ class Backup {
      * @param todo valid instance of class ToDo.
      * @return the encoded string.
      */
-    private static String encodeTags(String[] instanceData) {
+    private String encodeTags(String[] instanceData) {
 
         if (instanceData == null) {
             throw new IllegalArgumentException("The ToDos' string representation must be at least empty.");
@@ -67,5 +82,29 @@ class Backup {
             encodedTags = encodedTags + "\"" + instanceData[i] + "\" ";
         }
         return encodedTags + "]\n";
+    }
+
+
+    /**
+     * RETURN a ToDoList composed of the Objects found in the backup file
+     * @return a valid ToDoList
+     * @throws FileNotFoundException if it doesn't file any backup file
+     * @throws IllegalBackupException if the backup file is corrupted
+     */
+    private void retrieveBackup() {}
+
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ToDo myTodo = new ToDo("Titolo", "Descrizione", Arrays.asList("tag1", "tag2"));
+        try {
+            objectMapper.writeValue(new File("laMiaProva.json"), myTodo);
+            System.out.println("FATTO!");
+        } catch(JsonProcessingException jpe) {
+            System.out.println("FUCK");
+            jpe.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("SUPER FUCK");
+            ioe.printStackTrace();
+        }
     }
 }
