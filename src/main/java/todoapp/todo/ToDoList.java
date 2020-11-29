f15cccbdcb90020be87dce162c55bf44ec778c4d
dambrosidenis <73072997+dambrosidenis@users.noreply.github.com>
2020-11-29 15:07:49 +0100

    Update ToDoList
    Added iterator interface

--- src/main/java/todoapp/todo/ToDoList.java
+++ src/main/java/todoapp/todo/ToDoList.java
@@ -1,10 +1,13 @@
 package todoapp.todo;
 
 import todoapp.exceptions.EmptyFieldException;
+import todoapp.exceptions.InvalidFieldException;
+
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.ArrayList;
 
-class ToDoList {
+class ToDoList implements Iterable<ToDo> {
 
     private ArrayList<ToDo> loadedToDos;
     private final String emptyTitleErrorString = "A ToDo title can't be empty";
@@ -15,12 +18,16 @@ class ToDoList {
 
     /**
      * MODIFY loadedToDos by adding a new ToDo instance.
-     * @param toDoTitle is the title of the ToDo. Required not empty.
-     * @param toDoDescription is the description of the ToDo. At least it has to be an empty String.
-     * @param toDoTags is the list of tags of the ToDo. At least it has to be empty.
-     * @throws EmptyStringException when the ToDo title is empty. In this case the ToDo wouldn't be added.
+     * 
+     * @param toDoTitle       is the title of the ToDo. Required not empty.
+     * @param toDoDescription is the description of the ToDo. At least it has to be
+     *                        an empty String.
+     * @param toDoTags        is the list of tags of the ToDo. At least it has to be
+     *                        empty.
+     * @throws EmptyFieldException  when the ToDo title is empty. In this case the
+     *                              ToDo wouldn't be added.
      */
-    void addToDo(String toDoTitle, String toDoDescription, Collection<String> toDoTags) throws EmptyFieldException{
+    void addToDo(String toDoTitle, String toDoDescription, Collection<String> toDoTags) throws EmptyFieldException {
 
         if (toDoTitle == null || toDoTitle.isEmpty()) {
             throw new EmptyFieldException(emptyTitleErrorString);
@@ -28,12 +35,37 @@ class ToDoList {
 
         loadedToDos.add(new ToDo(toDoTitle, toDoDescription, toDoTags));
     }
-    
+
     /**
      * MODIFY loadedToDos by adding a new ToDo instance.
-     * @param toDoTitle is the title of the ToDo. Required not empty.
-     * @param toDoDescription is the description of the ToDo. At least it has to be an empty String.
-     * @throws EmptyStringException when the ToDo title is empty. In this case the ToDo wouldn't be added.
+     * 
+     * @param toDoTitle       is the title of the ToDo. Required not empty.
+     * @param toDoDescription is the description of the ToDo. At least it has to be
+     *                        an empty String.
+     * @param toDoTags        is the list of tags of the ToDo. At least it has to be
+     *                        empty.
+     * @throws EmptyFieldException  when the ToDo title is empty. In this case the
+     *                              ToDo wouldn't be added.
+     */
+    void addToDo(String toDoTitle, String toDoDescription, String toDoCreation, Collection<String> toDoTags) throws EmptyFieldException {
+
+        if (toDoTitle == null || toDoTitle.isEmpty() || toDoCreation.isEmpty()) {
+            throw new EmptyFieldException(emptyTitleErrorString);
+        }
+
+        loadedToDos.add(new ToDo(toDoTitle, toDoDescription, toDoCreation, toDoTags));
+    }
+
+
+
+    /**
+     * MODIFY loadedToDos by adding a new ToDo instance.
+     * 
+     * @param toDoTitle       is the title of the ToDo. Required not empty.
+     * @param toDoDescription is the description of the ToDo. At least it has to be
+     *                        an empty String.
+     * @throws EmptyFieldException  when the ToDo title is empty. In this case the
+     *                              ToDo wouldn't be added.
      */
     void addToDo(String toDoTitle, String toDoDescription) throws EmptyFieldException {
 
@@ -46,7 +78,9 @@ class ToDoList {
 
     /**
      * MODIFY loadedToDos by deleting a given ToDo instance.
-     * @param deletedToDo is the index of the instance that has to be removed. Required a valid instance contained in loadedToDos.
+     * 
+     * @param deletedToDo is the index of the instance that has to be removed.
+     *                    Required a valid instance contained in loadedToDos.
      */
     void removeToDo(int deletedToDoIndex) {
 
@@ -60,9 +94,13 @@ class ToDoList {
 
     /**
      * MODIFY a ToDo by changing its title.
-     * @param modifiedToDo is the index of the instance that has to be modified. Required a valid instance contained in loadedToDos.
-     * @param newTitle is the modified title of the todo. Required a not empty String.
-     * @throws EmptyFieldException when the ToDo title is empty. In this case the ToDo wouldn't be modified.
+     * 
+     * @param modifiedToDo is the index of the instance that has to be modified.
+     *                     Required a valid instance contained in loadedToDos.
+     * @param newTitle     is the modified title of the todo. Required a not empty
+     *                     String.
+     * @throws EmptyFieldException when the ToDo title is empty. In this case the
+     *                             ToDo wouldn't be modified.
      */
     void modifyToDoTitle(int modifiedToDoIndex, String newTitle) throws EmptyFieldException {
 
@@ -78,10 +116,12 @@ class ToDoList {
 
     /**
      * MODIFY a ToDo by changing its description.
-     * @param modifiedToDo is the index of the instance that has to be modified. Required a valid instance contained in loadedToDos.
+     * 
+     * @param modifiedToDo   is the index of the instance that has to be modified.
+     *                       Required a valid instance contained in loadedToDos.
      * @param newDescription is the modified description of the todo.
      */
-    void modifyToDoDescription(int modifiedToDoIndex, String newDescription){
+    void modifyToDoDescription(int modifiedToDoIndex, String newDescription) {
 
         if (modifiedToDoIndex < 0 || modifiedToDoIndex >= loadedToDos.size()) {
             throw new IndexOutOfBoundsException();
@@ -93,11 +133,15 @@ class ToDoList {
 
     /**
      * MODIFY a ToDo by adding a tag to its list if not already in.
-     * @param modifiedToDo is the intex of the instance that has to be modified. Required a valid instance contained in loadedToDos.
-     * @param newTag is the new tag of the todo.
-     * @throws EmptyFieldException when newTag is null or empty. In this case the tag wouldn't be added.
+     * 
+     * @param modifiedToDo is the intex of the instance that has to be modified.
+     *                     Required a valid instance contained in loadedToDos.
+     * @param newTag       is the new tag of the todo.
+     * @throws EmptyFieldException   when newTag is null or empty. In this case the
+     *                               tag wouldn't be added.
+     * @throws InvalidFieldException
      */
-    void addToDoTag(int modifiedToDoIndex, String newTag) throws EmptyFieldException {
+    void addToDoTag(int modifiedToDoIndex, String newTag) throws EmptyFieldException, InvalidFieldException {
 
         if (modifiedToDoIndex < 0 || modifiedToDoIndex >= loadedToDos.size()) {
             throw new IndexOutOfBoundsException();
@@ -105,16 +149,22 @@ class ToDoList {
             throw new EmptyFieldException("A ToDo tag that is going to be added can't be empty");
         }
 
-        ToDo modifiedToDo = recogniseToDo(modifiedToDoIndex);
-        if (!modifiedToDo.getTags().contains(newTag)) {
-            modifiedToDo.addTag(newTag);
+        try {
+            ToDo modifiedToDo = recogniseToDo(modifiedToDoIndex);
+            if (!modifiedToDo.getTags().contains(newTag)) {
+               modifiedToDo.addTag(newTag);
+            }
+        } catch (InvalidFieldException ife) {
+            throw ife;
         }
     }
 
     /**
      * MODIFY a ToDo by deleting a tag from its list if it already exists.
-     * @param modifiedToDo is the index of the instance that has to be modified. Required a valid instance contained in loadedToDos.
-     * @param deletedTag is the tag of the todo that has to be deleted.
+     * 
+     * @param modifiedToDo is the index of the instance that has to be modified.
+     *                     Required a valid instance contained in loadedToDos.
+     * @param deletedTag   is the tag of the todo that has to be deleted.
      */
     void deleteToDoTag(int modifiedToDoIndex, String deletedTag) {
 
@@ -127,20 +177,23 @@ class ToDoList {
             modifiedToDo.deleteTag(deletedTag);
         }
     }
-    
+
     /**
      * Get a ToDo based on its index in loadedToDos
+     * 
      * @param toDoIndex is the index of the desired ToDo.
      * @return the reference to the ToDo.
      */
     private ToDo recogniseToDo(int toDoIndex) {
         return loadedToDos.get(toDoIndex);
     }
-    
+
     /**
      * Get all data about a ToDo.
+     * 
      * @param toDoIndex is the index of the desired ToDo.
-     * @return a string array containing, in order: the title, the description, the date of creation and the tags of the ToDo.
+     * @return a string array containing, in order: the title, the description, the
+     *         date of creation and the tags of the ToDo.
      */
     String[] getToDoData(int toDoIndex) {
 
@@ -151,13 +204,24 @@ class ToDoList {
         ToDo spotlightedToDo = recogniseToDo(toDoIndex);
         return spotlightedToDo.getData();
     }
-    
-    
+
     /**
      * Get the number of elements contained in loadedToDos.
+     * 
      * @return the equivalent to loadedToDos' size.
      */
     int size() {
         return loadedToDos.size();
+    }
+
+
+    /**
+     * Return an iterator interface for the list of ToDos currently active
+     * 
+     * @return the iterator.
+     */
+    @Override
+    public Iterator<ToDo> iterator() {
+        return loadedToDos.iterator();
     }
 }   // class ToDoList
